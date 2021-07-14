d760132be98f7163ee8ce8e2a989ae94d28a5c6f
Matthew Hanrahan <sunracer1977@gmail.com>
2021-07-14 10:05:46 -0400

    moved contents to root dir

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import AnimeScreen from './app/Screen/animeScreen';
 import Post from './app/Components/Post/Post';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Post />
+     <AnimeScreen/>
       {/* <Post /> */}   
 
     </View>
