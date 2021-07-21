d335e6e15007c905bab5b3fc461c8a800a9fd691
Nick Wang <nick.wang.uu@gmail.com>
2021-07-21 10:20:02 -0600

    stable

--- app/Navigation/MainRoutes.tsx
+++ app/Navigation/MainRoutes.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../Screen/Login';
 import PostScreen from '../Screen/PostScreen';
 
-
+import Navbar from './NavBar';
 import AnimeScreen from '../Screen/animeScreen';
 import ProfilePage from '../Components/Profile/ProfilePage';
 
-
+import SideMenu from 'react-native-side-menu-updated';
 import animeScreen from '../Screen/animeScreen';
+import { TouchableOpacity, View } from 'react-native';
 
 
 interface RouterProps
@@ -20,21 +21,26 @@ const Stack = createStackNavigator();
 
 const MainRoutes: React.FC<RouterProps> = (props:RouterProps) =>
 {
+    const menu =<View></View>;
+    const [open, setOpen] = useState<boolean>(false);
+    const openMenu = () => {setOpen(true)};
     return (
         <NavigationContainer >
 
+            <SideMenu menu = {menu} isOpen= {open} onChange={isOpen => setOpen(isOpen)}>
+                <Stack.Navigator initialRouteName='Login'>
 
-            <Stack.Navigator initialRouteName='Login'>
 
-
-                <Stack.Screen
-                name="Login"
-                component={Login}
-                />
-                <Stack.Screen name="Post" component={PostScreen} />
-                <Stack.Screen name="Anime" component={AnimeScreen} />
-                <Stack.Screen name="User" component={ProfilePage} />
-            </Stack.Navigator>
+                    <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{ headerTitle: props => <View><Navbar menu = {openMenu}></Navbar></View> }}
+                    />
+                    <Stack.Screen name="Post" component={PostScreen} />
+                    <Stack.Screen name="Anime" component={AnimeScreen} />
+                    <Stack.Screen name="User" component={ProfilePage} />
+                </Stack.Navigator>
+            </SideMenu>
         </NavigationContainer>
         
     );
