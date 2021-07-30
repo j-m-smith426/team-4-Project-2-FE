96ed65eb2821e42991285f875b9747263c596bc8
Nick Wang <nick.wang.uu@gmail.com>
2021-07-30 10:02:18 -0600

    ahhh

--- app/redux/Actions.ts
+++ app/redux/Actions.ts
@@ -1,27 +1,27 @@
 export enum LoginActions {
-  LOGIN = "Log In",
-  LOGOUT = "Log out",
-}
-
-export enum CreatePostActions {
-  CREATE = "Create Post",
-  ADD = "Add Comment",
-  Load = "Load Post",
-}
-
-export enum SwitchPageAction {
-  UPDATE = "update page",
-  VIEW_POST = "view post",
-}
-
-export interface ISwitchPageActions {
-  type: SwitchPageAction;
-  payload: { name: string; parentID: string; postID: string };
-}
-
-export interface ILoginActions {
-  type: LoginActions;
-  payload: { name: string; type: string };
-}
-
-export interface IAppActions extends ILoginActions {}
+    LOGIN = "Log In",
+    LOGOUT = "Log out",
+  }
+  
+  export enum CreatePostActions {
+    CREATE = "Create Post",
+    ADD = "Add Comment",
+    Load = "Load Post",
+  }
+  
+  export enum SwitchPageAction {
+    UPDATE = "update page",
+    VIEW_POST = "view post",
+  }
+  
+  export interface ISwitchPageActions {
+    type: SwitchPageAction;
+    payload: { name: string; parentID: string; postID: string };
+  }
+  
+  export interface ILoginActions {
+    type: LoginActions;
+    payload: { name: string; type: string };
+  }
+  
+  export interface IAppActions extends ILoginActions {}
